c39a4c28f10b7a9e27ab17e7ef731be8af91bcaa
kpa <billkp@yeah.net>
2022-12-03 18:41:17 +0800

    音视频C\C++层学习项目

--- media_project/main.cpp
+++ media_project/main.cpp
@@ -7,8 +7,13 @@
 
 #include <iostream>
 
+int test() {
+    return 100;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    test();
     return 0;
 }
