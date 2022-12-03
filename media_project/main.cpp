daf8cdd7b62cad968051ff5b91c569a49565fa41
mtkj <mtkj@mtkjdeMacBook-Pro.local>
2022-12-03 18:34:50 +0800

    音视频C\C++层学习项目

---
+++ media_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  media_project
+//
+//  Created by mtkj on 2022/12/3.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
